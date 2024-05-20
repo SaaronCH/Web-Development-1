e29462bd103d5de1e4e1ff8305f2fe65ba28c146
SaaronCH <optoop9@gmail.com>
2024-05-20 10:48:55 +0545

    Forward, Backward,Play/Pause along with progressbar updated

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("Welcome To Spotlify");
 let songsIndex = 0;
 let audioElement = new Audio('songs/SmackThat.mp3');
-let masterPlay = document.getElementById('masterPlay')
-let progressBar = document.getElementById('progressBar')
-
+let masterPlay = document.getElementById('masterPlay');
+let progressBar = document.getElementById('progressBar');
+let playing =document.getElementById('playing');
 let songs = [
     { songName: "Smack That", filePath: "F:\Assignment 1\songs\SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
-    /*{ songName: "Smack That", filePath: "songs/SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
-    { songName: "Smack That", filePath: "songs/SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
+    /*{ songName: "Beautiful", filePath: "F:\Assignment 1\songs\Beautiful.mp3", coverPath: "covers\Beautiful.png" },
+    *{ songName: "Smack That", filePath: "songs/SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
     { songName: "Smack That", filePath: "songs/SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
     { songName: "Smack That", filePath: "songs/SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
     { songName: "Smack That", filePath: "songs/SmackThat.mp3", coverPath: "covers/smackThat.jpg" },
@@ -21,6 +21,7 @@ masterPlay.addEventListener('click', ()=>{
             audioElement.play();
             masterPlay.classList.remove('fa-circle-play');
             masterPlay.classList.add('fa-circle-pause');
+            playing.style.opacity = 1;
             
         }
     else
@@ -28,9 +29,13 @@ masterPlay.addEventListener('click', ()=>{
         audioElement.pause();
         masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play');
+        playing.style.opacity = 0;
     }
 })
 progressBar.addEventListener('timeupdate', ()=>{
-    console.log('timeupdate');
+console.log('timeupdate',progressBar );
 
+let progressBar = parseInt((audio.currentTime/audio.duration)*100);
+    console.log('timeupdate',progressBar);
+    
 })
